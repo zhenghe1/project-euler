ee531a613cc94c36c7d24f19586e8650222d8f05
Zheng He <zhenghe12613@gmail.com>
2014-08-02 21:29:04 +0000

    Added a comment.

--- p008.h
+++ p008.h
@@ -1,4 +1,5 @@
 /* Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product? */
+// Dynamic programming approach.
 #include "euler_interface.h"
 #include <deque>
 
